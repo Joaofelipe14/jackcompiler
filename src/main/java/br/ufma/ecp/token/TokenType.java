8ecb0565e71ac639a2487660649385a97a2e76c9
carlosCajado <cajadomotos@gmail.com>
2024-02-08 15:57:30 -0300

    :sparkles: feat: (symbol update)

--- src/main/java/br/ufma/ecp/token/TokenType.java
+++ src/main/java/br/ufma/ecp/token/TokenType.java
@@ -12,17 +12,23 @@ public enum TokenType {
 
 
      IDENT,
-
- 
-     // keywords
-     WHILE, CLASS,CONSTRUCTOR,FUNCTION,
-     METHOD,FIELD,STATIC,VAR,INT,
-     CHAR,BOOLEAN,VOID,TRUE,FALSE,
-     NULL,THIS,LET,DO,IF,ELSE, RETURN,
-
      EOF,
+ 
 
-     ILLEGAL;
+    // symbols
+    LPAREN,RPAREN,
+    LBRACE, RBRACE,
+    LBRACKET,RBRACKET,
+
+    COMMA, SEMICOLON, DOT,ASTERISK, SLASH,
+    AND, OR, NOT,   
+    LT, GT, EQ,
+    // keywords
+    WHILE, CLASS,CONSTRUCTOR,FUNCTION,
+    METHOD,FIELD,STATIC,VAR,INT,
+    CHAR,BOOLEAN,VOID,TRUE,FALSE,
+    NULL,THIS,LET,DO,IF,ELSE, RETURN,
+    ILLEGAL;
 
      static public boolean isSymbol (char c) {
         String symbols = "{}()[].,;+-*/&|<>=~";
