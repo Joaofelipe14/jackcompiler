85a60e798dcab62c336cccbc0b785ea8002c8702
carlosCajado <cajadomotos@gmail.com>
2024-02-19 21:17:29 -0300

    :wrench: feat(Whole literals)

---
+++ src/test/java/br/ufma/ecp/CodeGenerator.java
@@ -0,0 +1,27 @@
+package br.ufma.ecp;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import org.junit.Test;
+public class CodeGenerator  extends TestSupport{
+
+
+    @Test
+    public void testInt () {
+        var input = """
+            10
+            """;
+        
+        var parser = new Parser(input.getBytes(StandardCharsets.UTF_8));
+        parser.parseExpression();
+        String actual = parser.VMOutput();
+        String expected = """
+                push constant 10       
+                    """;
+            assertEquals(expected, actual);
+    }
+
+  
+}
